0e09fa8d083717fea6e7e5745504b8c7bc588c8a
MFarrell242 <farrell.michael.w@gmail.com>
2019-11-16 04:50:57 -0600

    can't get response but this is everything up to

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ $(document).ready(function(){
 $(presets).on("click", function(){
     console.log(this);
     let buttonName = $(this).val();
-    let lookup = "api.openweathermap.org/data/2.5/forecast?q=" + buttonName;
+    let lookup = "api.openweathermap.org/data/2.5/forecast?q=" + buttonName + "&appid=da7f60dbc2c04a78acbc54553c03b9c9";
     $.ajax({
         url: lookup,
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        // if I could get a response (which, what the fuck), this is where
+        // I'd assign values to cards and the main thingummy
     });
 });
 
@@ -27,11 +29,14 @@ $(search).on("click", function(){
     event.preventDefault();
     var citySearch = document.querySelector("#inputbox");
     citySearch = $(citySearch).val();
-    let lookup = "api.openweathermap.org/data/2.5/forecast?q=" + citySearch;
+    let lookup = "api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=da7f60dbc2c04a78acbc54553c03b9c9";
     $.ajax({
         url: lookup,
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        // if I could get a response (which, what the fuck), this is where
+        // I'd assign values to cards and the main thingummy, same as up there.
+        // dunno why it's not working =(
     });
 });
